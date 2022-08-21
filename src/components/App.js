2a9b6c24472507ae8b0936ae21224d6bb34d6ea1
raisle <override.256@gmail.com>
2022-08-21 15:46:19 +0200

    Set up UI, delete unused files

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React, {Component} from 'react';
+
+function App() {
+    return (<div className="App">
+            <h1>Hello world</h1>
+        </div>);
+}
+
+export default App;
